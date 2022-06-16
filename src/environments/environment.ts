9f6d12651a1378dac4ab1550914d31a33afa9843
name <prudhvi.kasina@gmail.com>
2022-06-16 19:24:41 +0530

    changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBbHW2ReY6LwpS0oFP_syDxelLIkPtrDjg",
+    authDomain: "my-ionic-app-52fcf.firebaseapp.com",
+    projectId: "my-ionic-app-52fcf",
+    storageBucket: "my-ionic-app-52fcf.appspot.com",
+    messagingSenderId: "922158082344",
+    appId: "1:922158082344:web:a515f4846e6859a24b481d",
+    measurementId: "G-XMWYVS2H9P"
+  },
+};
